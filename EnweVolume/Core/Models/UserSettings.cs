05e3cb530e8415ce21a674d5b9a504e8917dcf0b
Alexander Vannovskiy <derpedcatto@protonmail.com>
2025-03-23 17:51:40 +0200

    Implementing `Equals` and `GetHashCode` for `UserSettings`

--- EnweVolume/Core/Models/UserSettings.cs
+++ EnweVolume/Core/Models/UserSettings.cs
@@ -15,4 +15,46 @@ public class UserSettings
     public bool StartWithSystemEnabled { get; set; }
     public string CurrentTheme { get; set; } = string.Empty;
     public string Locale { get; set; } = string.Empty;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        UserSettings other = (UserSettings)obj;
+        return AudioDeviceName == other.AudioDeviceName &&
+               VolumeRedThresholdValue == other.VolumeRedThresholdValue &&
+               VolumeYellowThresholdValue == other.VolumeYellowThresholdValue &&
+               NotificationRedPushEnabled == other.NotificationRedPushEnabled &&
+               NotificationRedSoundEnabled == other.NotificationRedSoundEnabled &&
+               NotificationRedSoundVolume == other.NotificationRedSoundVolume &&
+               NotificationYellowPushEnabled == other.NotificationYellowPushEnabled &&
+               NotificationYellowSoundEnabled == other.NotificationYellowSoundEnabled &&
+               NotificationYellowSoundVolume == other.NotificationYellowSoundVolume &&
+               ChangeProgressBarColorEnabled == other.ChangeProgressBarColorEnabled &&
+               StartWithSystemEnabled == other.StartWithSystemEnabled &&
+               CurrentTheme == other.CurrentTheme &&
+               Locale == other.Locale;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(
+            AudioDeviceName,
+            VolumeRedThresholdValue,
+            VolumeYellowThresholdValue,
+            NotificationRedPushEnabled,
+            NotificationRedSoundEnabled,
+            NotificationRedSoundVolume,
+            HashCode.Combine(
+                NotificationYellowPushEnabled,
+                NotificationYellowSoundEnabled,
+                NotificationYellowSoundVolume,
+                ChangeProgressBarColorEnabled,
+                StartWithSystemEnabled,
+                CurrentTheme,
+                Locale
+            )
+        );
+    }
 }
