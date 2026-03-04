18c55e05ceebe27195415d3add1d682d5923a034
derpedcatto <derpedcatto@protonmail.com>
2026-03-04 18:57:01 +0100

    refactor: simplifying result class

--- EnweVolume/Core/Models/Result.cs
+++ EnweVolume/Core/Models/Result.cs
@@ -2,24 +2,34 @@
 
 namespace EnweVolume.Core.Models;
 
-public sealed class Result<T, TError>
+public sealed class Result
+{
+    [MemberNotNullWhen(false, nameof(Error))]
+    public bool IsSuccess { get; }
+    public Error? Error { get; }
+
+    private Result(bool isSuccess, Error? error) =>
+        (IsSuccess, Error) = (isSuccess, error);
+
+    public static Result Success() => new(true, default);
+    public static Result Failure(Error error) => new(false, error);
+}
+
+public sealed class Result<T>
 {
     [MemberNotNullWhen(true, nameof(Value))]
     [MemberNotNullWhen(false, nameof(Error))]
-    public bool IsSuccess { get; set; }
+    public bool IsSuccess { get; }
 
     public T? Value { get; }
-    public TError? Error { get; }
+    public Error? Error { get; }
 
-    private Result(bool isSuccess, T? value, TError? error) =>
+    private Result(bool isSuccess, T? value, Error? error) =>
         (IsSuccess, Value, Error) = (isSuccess, value, error);
 
-    public static Result<T, TError> Success(T value) =>
+    public static Result<T> Success(T value) =>
         new(true, value, default);
 
-    public static Result<T, TError> Failure(TError error) =>
+    public static Result<T> Failure(Error error) =>
         new(false, default, error);
-
-    public TResult Match<TResult>(Func<T, TResult> onSuccess, Func<TError, TResult> onFailure)
-        => IsSuccess ? onSuccess(Value) : onFailure(Error);
 }
